9af06ba8e9c34679acfa9ac685d94d28ef18f469
Rodrigo Santellan <rsantellan@gmail.com>
2015-01-09 14:53:08 -0200

    Fix listener.

--- Listener/DeleteListener.php
+++ Listener/DeleteListener.php
@@ -16,17 +16,12 @@ class DeleteListener {
         $entity = $args->getEntity();
         $entityManager = $args->getEntityManager();
         
-        if(class_exists("RSantellan\SitioBundle\Entity\Project"))
+
+        $query = $entityManager->createQuery("select a from MaithCommonAdminBundle:mAlbum a where a.object_id = :id and a.object_class = :object_class")->setParameters(array('id' => $entity->getId(), 'object_class' => get_class($entity)));
+        $albums = $query->getResult();
+        foreach($albums as $album)
         {
-            if(get_class($entity) == "RSantellan\SitioBundle\Entity\Project")
-            {
-                $query = $entityManager->createQuery("select a from MaithCommonAdminBundle:mAlbum a where a.object_id = :id and a.object_class = :object_class")->setParameters(array('id' => $entity->getId(), 'object_class' => get_class($entity)));
-                $albums = $query->getResult();
-                foreach($albums as $album)
-                {
-                    $entityManager->remove($album);
-                }
-            }
+            $entityManager->remove($album);
         }
     }
 }
