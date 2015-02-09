31707c87f210bed01d7dc07ad0d266b676613b5c
Rodrigo Santellan <rsantellan@gmail.com>
2015-02-09 09:25:52 -0200

    Change cotizador.

--- Model/BcuCotizadorData.php
+++ Model/BcuCotizadorData.php
@@ -19,20 +19,26 @@ class BcuCotizadorData {
     // 604800
   }
   
-  private function retrieveBcuCotization($urlToGo)
+  private function retrieveBcuCotization($urlToGo, $useCache = true)
   {
         $cacheData = $this->cachedata->get($urlToGo, $this->duration);
-        if($cacheData)
+        if($cacheData && $useCache)
           return $cacheData;
         $headers = get_headers($urlToGo);
         $response = substr($headers[0], 9, 3);
         if ($response == "404") {
             throw new \Exception('url not found: '.$urlToGo);
         }
-
+        
         $webdata = file_get_contents($urlToGo);
         $counter = 0;
-        $finalData = array();
+        $finalData = array(
+                'arbitrajes' => array(),
+                'cotizaciones' => array(
+                    'monedas' => array(),
+                    'ui' => array()
+                    ),
+                );
         foreach(preg_split("/((\r?\n)|(\r\n?))/", $webdata) as $line){
             // do stuff with $line
             
@@ -43,10 +49,50 @@ class BcuCotizadorData {
                 $objData->currency = trim(substr($line, 17, 16));
                 $objData->code = trim(substr($line, 40, 4));
                 $objData->exchange = str_replace(' ', '  ',trim(substr($line, 45)));
-                $finalData[$objData->code] = $objData; 
+                $finalData['arbitrajes'][$objData->code] = $objData; 
+            }
+            if(($counter > 37 && $counter < 44) || $counter == 47 )
+            {
+                $objData = new \stdClass();
+                $objData->country = trim(substr($line, 0, 16));
+                $objData->currency = trim(substr($line, 17, 16));
+                $objData->code = trim(substr($line, 40, 4));
+                $aux = str_replace(' ', '  ',trim(substr($line, 50)));
+                $exploded = explode(' ', $aux);
+                $objData->buy = $exploded[0];
+                $objData->sell = '';
+                $i = 1;
+                while($i < count($exploded))
+                {
+                    if($exploded[$i] != '')
+                    {
+                        $objData->sell = $exploded[$i];
+                    }
+                    $i++;
+                }
+                $finalData['cotizaciones']['monedas'][$objData->code] = $objData;
+            }
+            if($counter > 43 && $counter < 47)
+            {
+                if($counter == 44)
+                {
+                    $ui = array();
+                    $ui['values'] = array();
+                }
+                else
+                {
+                    $ui = $finalData['cotizaciones']['ui'];
+                }
+                $ui['country'] = trim(substr($line, 0, 16));
+                $ui['currency'] = trim(substr($line, 17, 16));
+                $ui['code'] = trim(substr($line, 40, 4));
+                $aux = explode(' ', trim(substr($line, 54, 61)));
+                $ui['values'][$aux[3]] = $aux[0];
+                $finalData['cotizaciones']['ui'] = $ui;
             }
             $counter ++;
         }
+        //var_dump($finalData['cotizaciones']['ui']);
         $this->cachedata->set($urlToGo, $finalData);
         return $finalData;     
     }
@@ -57,10 +103,10 @@ class BcuCotizadorData {
         return $this->retrieveBcuCotization(sprintf('http://bcu.gub.uy/Cotizaciones/oicot%s.txt', date('dmy')));
     }
     
-    public function retrieveLastUsableBcuCotizacion()
+    public function retrieveLastUsableBcuCotizacion($useCache = true)
     {
         $lastdate = $this->cachedata->get('lastusedurl', 86400);
-        if($lastdate)
+        if($lastdate && $useCache)
         {
           return $this->retrieveBcuCotization(sprintf('http://bcu.gub.uy/Cotizaciones/oicot%s.txt', $lastdate));
         }
@@ -71,7 +117,7 @@ class BcuCotizadorData {
         {
             try
             {
-                $data = $this->retrieveBcuCotization(sprintf('http://bcu.gub.uy/Cotizaciones/oicot%s.txt', $d1->format('dmy')));
+                $data = $this->retrieveBcuCotization(sprintf('http://bcu.gub.uy/Cotizaciones/oicot%s.txt', $d1->format('dmy')), $useCache);
                 $found = true;
                 $this->cachedata->set('lastusedurl', $d1->format('dmy'));
             }
